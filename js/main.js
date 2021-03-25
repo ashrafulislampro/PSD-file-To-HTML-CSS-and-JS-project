95b4260a1585c470f9050d6464d81416fa186a90
ashrafulislampro <ashrafulislam1659@gmail.com>
2021-03-25 06:36:43 -0700

    complete my psd file to html

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* bx slider content */
+
+$(document).ready(function(){
+          $('.slider_area').bxSlider();
+          $('#main-menu').meanmenu({
+                    meanMenuContainer: '#mobile_menu',
+                    meanScreenWidth: 991,
+          });
+});
